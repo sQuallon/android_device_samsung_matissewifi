2cb4926d46942daba115f8a45e924eea64277005
sub77 <sub77@ymail.com>
2015-04-09 02:26:38 +0200

    twrp config
    Change-Id: I69f0e3938fe9332ba3e58e0e60c859b89d2e80f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,6 +14,7 @@
 
 # Inherit from msm8226-common
 -include device/samsung/msm8226-common/BoardConfigCommon.mk
+#-include device/samsung/matissewifi/twrp/TWRPConfig.mk
 
 TARGET_OTA_ASSERT_DEVICE := matissewifi,matissewifixx
 
@@ -30,17 +31,16 @@ TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_matisse.c
 TARGET_UNIFIED_DEVICE := true
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK 		:= $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_BASE 			:= 0x00000000
-BOARD_KERNEL_CMDLINE 			:= console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
-#BOARD_KERNEL_CMDLINE 			:= console=null androidboot.console=null androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
-BOARD_KERNEL_PAGESIZE 			:= 2048
-BOARD_KERNEL_SEPARATED_DT 		:= true
-BOARD_MKBOOTIMG_ARGS 			:= --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
+BOARD_CUSTOM_BOOTIMG_MK 	:= $(LOCAL_PATH)/mkbootimg-lzma.mk
+BOARD_KERNEL_BASE 		:= 0x00000000
+BOARD_KERNEL_CMDLINE 		:= console=null androidboot.console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
+BOARD_KERNEL_PAGESIZE 		:= 2048
+BOARD_KERNEL_SEPARATED_DT 	:= true
+BOARD_MKBOOTIMG_ARGS 		:= --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
 TARGET_KERNEL_SOURCE := kernel/samsung/s3ve3g
-TARGET_KERNEL_CONFIG := cm-matissewifi_defconfig
-#TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_KERNEL_CONFIG := twrp-matissewifi_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := msm8226-sec_matissewifi_defconfig
+#TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 4096
@@ -49,15 +49,31 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15485760
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2097152000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12562627584
 
-#TARGET_KERNEL_CONFIG := twrp-matissewifi_defconfig
-#-include device/samsung/matissewifi/twrp/TWRPConfig.mk
-#BOARD_CUSTOM_BOOTIMG_MK 		:= $(LOCAL_PATH)/mkbootimg-lzma.mk
-TW_THEME := landscape_hdpi
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
-#TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/twrp.fstab
-
 # Releasetools
-#TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/matissewifi
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/matissewifi
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/samsung/matissewifi/rootdir/fstab.qcom
+DEVICE_RESOLUTION := 1280x800
+
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 126
+#TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+#TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_THEME := landscape_hdpi
+TW_INCLUDE_L_CRYPTO := false
+TW_USE_TOOLBOX := true
+TWHAVE_SELINUX := true
+
+TW_NO_USB_STORAGE := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+#TARGET_PREBUILT_KERNEL := device/htc/m8/recovery/kernel/kernel
+#TARGET_RECOVERY_INITRC := device/htc/m8/recovery/etc/init.rc
+
+#BOARD_RECOVERY_BLDRMSG_OFFSET := 2048
 
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
@@ -66,7 +82,6 @@ BOARD_SEPOLICY_DIRS += device/samsung/matissewifi/sepolicy
 
 BOARD_SEPOLICY_UNION += \
 		platform_app.te \
-		recovery.te
 
 # BlissPop Configs
 TARGET_TC_ROM := 4.8-sm
