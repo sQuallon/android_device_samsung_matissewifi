f10ee3b4cfc86e886bf9887c2b1808f0c7157eb4
sub77 <sub77@ymail.com>
2015-04-22 13:37:26 +0200

    Local Path

--- device.mk
+++ device.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := device/samsung/matissewifi
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
